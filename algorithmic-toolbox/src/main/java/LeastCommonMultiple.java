54b6b3691e9f99d31f39d06d28e729742199eafd
Michael Heinecke <michael.heinecke@booking.com>
2021-06-15 08:51:57 +0200

    Duplicate code from GreatestCommonDivisor as only single file can be submitted for grading

--- algorithmic-toolbox/src/main/java/LeastCommonMultiple.java
+++ algorithmic-toolbox/src/main/java/LeastCommonMultiple.java
@@ -3,9 +3,22 @@ import java.util.Scanner;
 public class LeastCommonMultiple {
     final static Scanner sc = new Scanner(System.in);
 
+    private static int applyEuclideanAlgorithm(int a, int b) {
+        return b == 0 ? a : applyEuclideanAlgorithm(b, a % b);
+    }
+
+    public static int calculateGreatestCommonDivisor(int a, int b) {
+        // constraint: 1 <= a, b <= 2 * 10^9
+        if (a == b) {
+            return a;
+        } else {
+            return a > b ? applyEuclideanAlgorithm(a, b) : applyEuclideanAlgorithm(b, a);
+        }
+    }
+
     public static long calculateLeastCommonMultiple(int a, int b) {
         // constraint: 1 <= a, b <= 10^7
-        return ((long) a * b) / GreatestCommonDivisor.calculateGreatestCommonDivisor(a, b);
+        return ((long) a * b) / calculateGreatestCommonDivisor(a, b);
     }
 
     public static void main(String[] args) {
