2b325890b4b2e5c0a5eeaa628e66ffd707531d18
Michael Heinecke <michael.heinecke@booking.com>
2021-08-16 16:30:17 +0200

    Replace Scanner with faster implementation based on InputStream

--- algorithmic-toolbox/src/main/java/MajorityElement.java
+++ algorithmic-toolbox/src/main/java/MajorityElement.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class MajorityElement {
-    static final Scanner sc = new Scanner(System.in);
+    static final FastScanner sc = new FastScanner(System.in);
 
     public static int findMajorityElement(int[] array) {
         Arrays.sort(array);  // O(n log n)
@@ -40,5 +44,33 @@ public class MajorityElement {
         }
 
         System.out.println(findMajorityElement(array));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
     }
 }
