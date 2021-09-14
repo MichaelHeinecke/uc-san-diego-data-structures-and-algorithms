75f3b25eafa111a9d1cfb7b41320c55b7a160973
Michael Heinecke <michael.heinecke@booking.com>
2021-09-14 17:06:03 +0200

    Rename tests

--- algorithmic-toolbox/src/test/java/PrimitiveCalculatorTest.java
+++ algorithmic-toolbox/src/test/java/PrimitiveCalculatorTest.java
@@ -7,22 +7,22 @@ import org.junit.jupiter.api.Test;
 public class PrimitiveCalculatorTest {
     // Constraint: 1 ≤ value ≤ 10^6
     @Test
-    void testFibonacci_InputMin() {
+    void testPrimitiveCalculator_InputMin() {
         assertEquals(Arrays.asList(1), PrimitiveCalculator.determineOptimalSequence(1));
     }
 
     @Test
-    void testFibonacci_InputFive() {
+    void testPrimitiveCalculator_InputFive() {
         assertEquals(Arrays.asList(1, 3, 4, 5), PrimitiveCalculator.determineOptimalSequence(5));
     }
 
     @Test
-    void testFibonacci_InputTen() {
+    void testPrimitiveCalculator_InputTen() {
         assertEquals(Arrays.asList(1, 3, 9, 10), PrimitiveCalculator.determineOptimalSequence(10));
     }
 
     @Test
-    void testFibonacci_Input_96234() {
+    void testPrimitiveCalculator_Input_96234() {
         assertEquals(Arrays.asList(1, 3, 9, 10, 11, 33, 99, 297, 891, 2673, 8019, 16038, 16039, 48117, 96234),
                 PrimitiveCalculator.determineOptimalSequence(96234));
     }
