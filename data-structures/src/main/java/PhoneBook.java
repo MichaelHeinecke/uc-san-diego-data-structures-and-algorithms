50434d45eb52bc874d919d42fc66d82c0ce31806
Michael Heinecke <michael.heinecke@booking.com>
2021-10-25 22:09:16 +0200

    Solution for phone book (hash table)

---
+++ data-structures/src/main/java/PhoneBook.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class PhoneBook {
+    private final FastScanner in = new FastScanner();
+    private final Map<Integer, String> contacts = new HashMap<>();
+
+    public static void main(String[] args) {
+        new PhoneBook().processQueries();
+    }
+
+    private Query readQuery() {
+        String type = in.next();
+        int number = in.nextInt();
+        if (type.equals("add")) {
+            String name = in.next();
+            return new Query(type, name, number);
+        } else {
+            return new Query(type, number);
+        }
+    }
+
+    void processQuery(Query query) {
+        if (query.getType().equals("add")) {
+            contacts.put(query.getNumber(), query.getName());
+        } else if (query.getType().equals("del")) {
+            contacts.remove(query.getNumber());
+        } else { // find query
+            System.out.println(contacts.getOrDefault(query.getNumber(), "not found"));
+        }
+    }
+
+    public void processQueries() {
+        int queryCount = in.nextInt();
+        for (int i = 0; i < queryCount; ++i)
+            processQuery(readQuery());
+    }
+
+    static class Query {
+        String type;
+        String name;
+        int number;
+
+        public Query(String type, String name, int number) {
+            this.type = type;
+            this.name = name;
+            this.number = number;
+        }
+
+        public Query(String type, int number) {
+            this.type = type;
+            this.number = number;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
