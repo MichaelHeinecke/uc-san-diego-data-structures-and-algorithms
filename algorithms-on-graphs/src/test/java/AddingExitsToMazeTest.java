5c7ba9139bcd271c9072dc2c71f399700b2b7958
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2022-04-20 21:28:36 +0200

    Remove variables not required

--- algorithms-on-graphs/src/test/java/AddingExitsToMazeTest.java
+++ algorithms-on-graphs/src/test/java/AddingExitsToMazeTest.java
@@ -10,8 +10,6 @@ public class AddingExitsToMazeTest {
         List<List<Integer>> adjacencyList = List.of(
                 List.of(1), List.of(0, 2), List.of(1), List.of()
         );
-        int mazeEntryVertex = 0;
-        int mazeExitVertex = 3;
 
         assertEquals(2, AddingExitsToMaze.findNumberOfConnectedComponents(adjacencyList));
     }
