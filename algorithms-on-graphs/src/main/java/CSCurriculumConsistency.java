3ef4453b9cccb75f1dd3562e5ed22d91f6801485
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2022-04-25 23:20:24 +0200

    Add solution for graph acyclicity problem "Checking Consistency of CS Curriculum"

---
+++ algorithms-on-graphs/src/main/java/CSCurriculumConsistency.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CSCurriculumConsistency {
+    static int isAcyclic(List<List<Integer>> adjacencyList) {
+        // use DFS to traverse graph while keeping track of
+        // mark all the vertices as not visited and not part of recursion stack
+        boolean[] visitedVertices = new boolean[adjacencyList.size()];
+        boolean[] recursionStack = new boolean[adjacencyList.size()];
+
+        // iterate over all vertices in graph and do DFS on the subtrees
+        for (int vertex = 0; vertex < adjacencyList.size(); vertex++)
+            if (isAcyclicUtil(adjacencyList, vertex, visitedVertices, recursionStack)) {
+                return 1;
+            }
+
+        return 0;
+    }
+
+    private static boolean isAcyclicUtil(List<List<Integer>> adjacencyList, int vertex,
+                                         boolean[] visitedVertices, boolean[] recursionStack) {
+        // if the current vertex is already on the stack, we discovered a back edge
+        if (recursionStack[vertex]) {
+            return true;
+        }
+
+        // if the current vertex is not currently on the stack but was visited previously,
+        // we already checked this subtree for back edges and there is no need to do it again
+        if (visitedVertices[vertex]) {
+            return false;
+        }
+
+        // mark the current node as visited and part of recursion stack
+        visitedVertices[vertex] = true;
+        recursionStack[vertex] = true;
+
+        List<Integer> neighbours = adjacencyList.get(vertex);
+        for (Integer neighbour : neighbours)
+            if (isAcyclicUtil(adjacencyList, neighbour, visitedVertices, recursionStack)) {
+                return true;
+            }
+
+        // mark vertex as not on stack before stepping out of method to previous vertex
+        recursionStack[vertex] = false;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        // empty adjacency list
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(i, new ArrayList<>());
+        }
+
+        // add directed edges
+        for (int i = 0; i < m; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            // vertex x has directed edge to vertex y
+            adjacencyList.get(x - 1).add(y - 1);
+        }
+
+        System.out.println(isAcyclic(adjacencyList));
+    }
+}
+
