ea3fc3d5ba6268cda697868e0b84962cd0a3f027
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2022-08-01 19:34:52 +0200

    Add logic to enable checking graphs with more than one connected component

--- algorithms-on-graphs/src/main/java/BipartiteGraph.java
+++ algorithms-on-graphs/src/main/java/BipartiteGraph.java
@@ -9,26 +9,31 @@ public class BipartiteGraph {
         NodeColour[] nodeColours = new NodeColour[adjacencyList.size()];
         IntStream.range(0, adjacencyList.size()).forEach(i -> nodeColours[i] = NodeColour.UNSET);
 
-        // Queue for breadth first search containing starting node.
-        Queue<Integer> bfsQueue = new LinkedList<>(Arrays.asList(0));
-        nodeColours[0] = NodeColour.BLACK;
+        // Iterate over all nodes as starting nodes to check disconnected parts of the graph.
+        for (int i = 0; i < adjacencyList.size(); i++) {
+            // Check if node was already visited previously and traverse with BFS if not.
+            if (nodeColours[i] == NodeColour.UNSET) {
+                // Queue for breadth first search containing starting node.
+                Queue<Integer> bfsQueue = new LinkedList<>(Arrays.asList(i));
 
-        while (!bfsQueue.isEmpty()) {
-            // Iterate over neighbouring vertices.
-            int nodeIndex = bfsQueue.remove();
+                while (!bfsQueue.isEmpty()) {
+                    // Iterate over neighbouring vertices.
+                    int nodeIndex = bfsQueue.remove();
 
-            for (int neighbouringNodeIndex : adjacencyList.get(nodeIndex)) {
-                // If not visited previously, colour will be unset.
-                if (nodeColours[neighbouringNodeIndex].equals(NodeColour.UNSET)) {
-                    // Enqueue node index as reference to its neighbours.
-                    bfsQueue.add(neighbouringNodeIndex);
-                    // Colour neighbouring node with opposite colour.
-                    NodeColour colour = nodeColours[nodeIndex].equals(NodeColour.BLACK) ? NodeColour.RED : NodeColour.BLACK;
-                    nodeColours[neighbouringNodeIndex] = colour;
-                }
-                // If colours of node and neighbouring node match, the graph is not bipartite.
-                else if (nodeColours[nodeIndex].equals(nodeColours[neighbouringNodeIndex])) {
-                    return 0;
+                    for (int neighbouringNodeIndex : adjacencyList.get(nodeIndex)) {
+                        // If not visited previously, colour will be unset.
+                        if (nodeColours[neighbouringNodeIndex].equals(NodeColour.UNSET)) {
+                            // Enqueue node index as reference to its neighbours.
+                            bfsQueue.add(neighbouringNodeIndex);
+                            // Colour neighbouring node with opposite colour.
+                            NodeColour colour = nodeColours[nodeIndex] == NodeColour.BLACK ? NodeColour.RED : NodeColour.BLACK;
+                            nodeColours[neighbouringNodeIndex] = colour;
+                        }
+                        // If colours of node and neighbouring node match, the graph is not bipartite.
+                        else if (nodeColours[nodeIndex] == nodeColours[neighbouringNodeIndex]) {
+                            return 0;
+                        }
+                    }
                 }
             }
         }
