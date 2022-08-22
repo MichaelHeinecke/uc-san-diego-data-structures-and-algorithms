b00dad379e656347559f0bc3298878e3a9ebef0a
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2022-08-22 14:38:46 +0200

    Tests and solution for graph problem "Computing The Minimum Cost Of A Flight" solved with Dijkstra's algorithm

---
+++ algorithms-on-graphs/src/main/java/MinimumCostOfFlight.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class MinimumCostOfFlight {
+
+    public static long determineMinimumCostBetweenVertices(List<List<Integer>> adjacencyList, List<List<Integer>> costs,
+                                                           int startVertex, int destinationVertex) {
+        // Implementation of Dijkstra's algorithm
+
+        // Distance from start vertex to vertex v
+        long[] distances = new long[adjacencyList.size()];
+        // Initialize distances as infinity
+        Arrays.fill(distances, Long.MAX_VALUE);
+        // Distance from start vertex to start vertex is 0
+        distances[startVertex] = 0;
+
+        // Previous vertex on optimal path
+        int[] previousVertices = new int[adjacencyList.size()];
+
+        // Create priority queue with distances as priority value
+        PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Vertex(startVertex, 0));
+
+        while (!priorityQueue.isEmpty()) {
+            int currentVertexId = priorityQueue.poll().vertexId;
+            List<Integer> adjacentVertices = adjacencyList.get(currentVertexId);
+            List<Integer> adjacentCosts = costs.get(currentVertexId);
+
+            for (int adjacencyIndex = 0; adjacencyIndex < adjacentVertices.size(); adjacencyIndex++) {
+                int adjacentVertexId = adjacentVertices.get(adjacencyIndex);
+                int adjacentCost = adjacentCosts.get(adjacencyIndex);
+
+                if (distances[adjacentVertexId] > distances[currentVertexId] + adjacentCost) {
+                    distances[adjacentVertexId] = distances[currentVertexId] + adjacentCost;
+                    previousVertices[adjacentVertexId] = currentVertexId;
+                    priorityQueue.add(new Vertex(adjacentVertexId, distances[adjacentVertexId]));
+                }
+            }
+        }
+
+        // Minimum distance from source vertex to destination vertex or -1 if not reachable
+        return distances[destinationVertex] != Long.MAX_VALUE ? distances[destinationVertex] : -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // Number of vertices
+        int m = scanner.nextInt(); // Number of edges
+
+        // Empty adjacency list and cost
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        List<List<Integer>> cost = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(i, new ArrayList<>());
+            cost.add(i, new ArrayList<>());
+        }
+
+        // Add neighbouring vertices
+        for (int i = 0; i < m; i++) {
+            int x, y, c;
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            c = scanner.nextInt();
+
+            adjacencyList.get(x - 1).add(y - 1);
+            cost.get(x - 1).add(c);
+        }
+
+        int startNode = scanner.nextInt() - 1;
+        int destinationNode = scanner.nextInt() - 1;
+        System.out.println(determineMinimumCostBetweenVertices(adjacencyList, cost, startNode, destinationNode));
+    }
+
+    static class Vertex implements Comparable<Vertex> {
+        public int vertexId;
+        public long cost;
+
+        public Vertex(int vertexId, long cost) {
+            this.vertexId = vertexId;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Vertex v) {
+            return Long.compare(this.cost, v.cost);
+        }
+    }
+
+}
---
+++ algorithms-on-graphs/src/test/java/MinimumCostOfFlightTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinimumCostOfFlightTest {
+
+    @Test
+    void whenVerticesAreNotConnected_ReturnMinusOne() {
+        List<List<Integer>> adjacencyList = List.of(
+                List.of(1, 2), List.of(2), List.of()
+        );
+        List<List<Integer>> cost = List.of(
+                List.of(7), List.of(5), List.of(2)
+        );
+        int startVertex = 2;
+        int destinationVertex = 1;
+
+        assertEquals(-1, MinimumCostOfFlight.determineMinimumCostBetweenVertices(adjacencyList, cost,
+                startVertex, destinationVertex));
+    }
+
+    @Test
+    void whenUniqueShortestPathExists_ReturnMinimumCost() {
+        List<List<Integer>> adjacencyList = List.of(
+                List.of(1, 2), List.of(2), List.of(), List.of(0)
+        );
+        List<List<Integer>> costs = List.of(
+                List.of(1, 5), List.of(2), List.of(), List.of(2)
+        );
+        int startVertex = 0;
+        int destinationVertex = 2;
+
+        assertEquals(3, MinimumCostOfFlight.determineMinimumCostBetweenVertices(adjacencyList, costs,
+                startVertex, destinationVertex));
+    }
+
+    @Test
+    void whenMultipleShortestPathExists_ReturnMinimumCost() {
+        List<List<Integer>> adjacencyList = List.of(
+                List.of(1, 2), List.of(2, 3, 4), List.of(1, 4, 3), List.of(), List.of(3)
+        );
+        List<List<Integer>> costs = List.of(
+                List.of(4, 2), List.of(2, 2, 3), List.of(1, 4, 4), List.of(), List.of(1)
+        );
+        int startVertex = 0;
+        int destinationVertex = 4;
+
+        assertEquals(6, MinimumCostOfFlight.determineMinimumCostBetweenVertices(adjacencyList, costs,
+                startVertex, destinationVertex));
+    }
+
+}
