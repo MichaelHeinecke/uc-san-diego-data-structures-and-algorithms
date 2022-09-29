08e32253986cda1a068245a8dad3732ecadb1498
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2022-09-29 16:01:19 +0200

    Optimized solution for inverting BWT

--- algorithms-on-strings/src/main/java/ReconstructStringFromBurrowsWheelerTransform.java
+++ algorithms-on-strings/src/main/java/ReconstructStringFromBurrowsWheelerTransform.java
@@ -1,29 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.NoSuchElementException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ReconstructStringFromBurrowsWheelerTransform {
+
     public String reconstructStringFromBurrowsWheelerTransform(String bwt) {
         // The Burrows Wheeler Transform (BWT) is the last column of the BWT matrix
-        // The original string can be reconstructed iteratively by adding the BWT to the matrix
-        // and sorting the rows length(bwt) times
-        String[] bwtMatrix = new String[bwt.length()];
-        Arrays.fill(bwtMatrix, "");
+        Map<Character, Integer> charCounts = new HashMap<>();
+        List<Integer> charRanks = new ArrayList<>();
 
-        for (int i = 0; i < bwt.length(); i++) {
-            for (int j = 0; j < bwt.length(); j++) {
-                bwtMatrix[j] = bwt.charAt(j) + bwtMatrix[j];
-            }
-            Arrays.sort(bwtMatrix);
+        for (char c : bwt.toCharArray()) {
+            int count = charCounts.getOrDefault(c, 0);
+            charRanks.add(count);
+            charCounts.put(c, ++count);
         }
 
-        // There will be exactly one element in the array ending with "$"
-        return Arrays.stream(bwtMatrix)
-                .filter(e -> e.endsWith("$"))
-                .findFirst()
-                .orElseThrow(NoSuchElementException::new);
+        Map<Character, Integer> firstColumn = new HashMap<>();
+        int charRange = 0;
+
+        List<Map.Entry<Character, Integer>> sortedCharCounts = charCounts.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toList());
+
+        for (Map.Entry<Character, Integer> e : sortedCharCounts) {
+            firstColumn.put(e.getKey(), charRange);
+            charRange = charRange + e.getValue();
+        }
+
+        StringBuilder reconstructed = new StringBuilder("$");
+        int rowIndex = 0;
+        while (bwt.charAt(rowIndex) != '$') {
+            char c = bwt.charAt(rowIndex);
+            reconstructed.insert(0, c);
+            // jump to row that starts with character of same rank
+            rowIndex = firstColumn.get(c) + charRanks.get(rowIndex);
+        }
+
+        return reconstructed.toString();
     }
 
     public static void main(String[] args) throws IOException {
