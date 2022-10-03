4d2e1e767998a4b31351011e740d9ccdb462759b
Michael Heinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2022-10-03 14:45:57 +0200

    Replace method

--- algorithms-on-strings/src/test/java/ReconstructStringFromBurrowsWheelerTransformTest.java
+++ algorithms-on-strings/src/test/java/ReconstructStringFromBurrowsWheelerTransformTest.java
@@ -36,7 +36,7 @@ public class ReconstructStringFromBurrowsWheelerTransformTest {
                     Arguments.of("AC$A", "ACA$"),
                     Arguments.of("AGGGAA$", "GAGAGA$"),
                     Arguments.of("do$oodwg", "dogwood$"),
-                    Arguments.arguments("e$elplepa", "appellee$"),
+                    Arguments.of("e$elplepa", "appellee$"),
                     Arguments.of("torg$_hntriatnet_wo_sb", "another_string_to_bwt$"),
                     Arguments.of("G$AAACAAAAAACCACCCAAAGCGACCACACCCTGGGTTTTTTCTTGGGGTTGTC", "AAAAAAAACCCCCTTTTGTGTGTGTGTTTGGGGCCCAAAACACACACCTGCACG$"),
                     Arguments.of("T$AAGGGTAGTCCCCCCCGAAAAAGCGGGGGGGAGGGTTTTTTTTTTTTGTG", "AAAAGGGGGGGTTTTTTTTCCCCCCCCGGGGGTTTTTTTAGGAGAGAGAGT$")
